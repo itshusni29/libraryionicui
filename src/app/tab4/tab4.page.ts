44386c173d857061089ff41d44b38eafaee973a8
Muhammad Husni <if22.muhammadhusni@mhs.ubpkarawang.ac.id>
2024-06-15 16:29:49 +0700

    display phto profile in user tabs

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tab4',
@@ -10,8 +11,14 @@ import { Router } from '@angular/router';
 })
 export class Tab4Page implements OnInit {
   user: any = {};
+  safePhotoUrl: SafeUrl | undefined;
 
-  constructor(private userService: UserService, private authService: AuthService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService,
+    private router: Router,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit() {
     this.loadUserData();
@@ -21,9 +28,15 @@ export class Tab4Page implements OnInit {
     this.userService.getUserData().subscribe(
       (data: any) => {
         this.user = data;
+        if (this.user.photo_profile) {
+          this.safePhotoUrl = this.sanitizer.bypassSecurityTrustUrl(`http://127.0.0.1:8000/storage/${this.user.photo_profile}`);
+        } else {
+          this.safePhotoUrl = undefined; // Atau berikan nilai default lainnya jika perlu
+        }
       },
       (error: any) => {
         console.error('Error fetching user data', error);
+        // Handle error securely, avoid exposing sensitive information
       }
     );
   }
@@ -36,9 +49,7 @@ export class Tab4Page implements OnInit {
       },
       (error: any) => {
         console.error('Logout failed', error);
-        // Handle logout failure
-        // For example, you can still clear local storage and redirect to login page
-        localStorage.removeItem('token');
+        localStorage.removeItem('token'); // Hapus token meskipun logout gagal
         this.router.navigate(['/login']);
       }
     );
